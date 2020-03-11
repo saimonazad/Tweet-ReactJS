a84fc6c9bcdcfac35178afcd21f812817c769b81
Alauddin Al Azad <saimon.azad@hotmail.com>
2020-03-11 16:01:52 +0600

    Clock Component updated
    * made clock updating after a certain period

--- src/component/ClockComponent/ClockComponent.js
+++ src/component/ClockComponent/ClockComponent.js
@@ -1,16 +1,35 @@
 import React, { Component } from "react";
 
 export default class ClockComponent extends Component {
-  render() {
-    const currentTime = new Date(),
-      hours = currentTime.getHours(),
-      minutes = currentTime.getMinutes(),
-      seconds = currentTime.getSeconds(),
-      ampm = hours > 12 ? "pm" : "am";
+  constructor(props) {
+    super(props);
+    this.state = this.getTime();
+  }
+  componentDidMount() {
+    this.setTimer();
+  }
+  updateClock() {
+    this.setState(this.getTime, this.setTimer);
+  }
+  setTimer() {
+    clearTimeout(this.timeout);
+    this.timeout = setTimeout(this.updateClock.bind(this), 1000);
+  }
 
+  getTime() {
+    const currentTime = new Date();
+    return {
+      hours: currentTime.getHours(),
+      minutes: currentTime.getMinutes(),
+      seconds: currentTime.getSeconds(),
+      ampm: currentTime.getHours() > 12 ? "pm" : "am"
+    };
+  }
+  render() {
+    const { hours, minutes, seconds, ampm } = this.state;
     return (
       <div className="clock">
-        {hours == 0 ? 12 : hours > 12 ? hours - 12 : hours}:
+        {hours === 0 ? 12 : hours > 12 ? hours - 12 : hours}:
         {minutes > 9 ? minutes : `0${minutes}`}:
         {seconds > 9 ? seconds : `0${seconds}`}:{ampm}
       </div>
